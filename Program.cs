387c68e2dfa34191c4c8174da58b952ec3b77b09
Kento Kawakami <kawakamikento@gmail.com>
2020-05-25 19:08:40 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02_03_FirstBankOfSuncoast
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to C#");
+    }
+  }
+}
